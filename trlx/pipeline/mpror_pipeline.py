095e2021f187d9a4d7246b89eeac14ae8bab4e79
Kushal Arora <kushal.arora@tri.global>
2023-11-24 10:29:44 -0800

    fix for mpror rollout.

--- trlx/pipeline/mpror_pipeline.py
+++ trlx/pipeline/mpror_pipeline.py
@@ -2,6 +2,7 @@
 
 from typing import Any, Dict, List, Union
 import numpy as np
+import torch
 from transformers import PreTrainedTokenizer
 from torch.utils.data import DataLoader
 from trlx.models.modeling_mpror import MPRORConfig
@@ -40,6 +41,7 @@ class MPRORPipeline(BasePipeline):
         config: MethodConfig = None, 
         is_eval: bool = False,
         is_seq2seq: bool = False,
+        add_whitespace_to_label: bool = False,
     ):
         super().__init__()
 
@@ -57,6 +59,7 @@ class MPRORPipeline(BasePipeline):
         self.add_special_tokens = add_special_tokens
         self.total_steps = total_steps
         self.curr_step = -1
+        self.add_whitespace_to_label = add_whitespace_to_label
 
         metadata = samples
         prompts = [x["prompt"] for x in metadata]
@@ -87,11 +90,11 @@ class MPRORPipeline(BasePipeline):
 
     def create_loader(self, batch_size: int, shuffle=False, sampler=None, drop_last=False) -> DataLoader:
         def collate_fn(xs):
-            labels = [" "  + x["label"] for x in xs]
+            if self.add_whitespace_to_label:
+                labels = [" "  + x["label"] for x in xs]
+            else:
+                labels = [x['label'] for x in xs]
             self.curr_step += 1
-            
-            model_labels = self.tokenizer(labels, truncation=True, padding=False, 
-                            max_length=self.config.max_rollin_length, add_special_tokens=self.add_special_tokens,)
 
             prompts_tokens = [x["input_ids"] for x in xs]
             indexes = [0] * len(prompts_tokens)
@@ -154,6 +157,70 @@ class MPRORPipeline(BasePipeline):
             for key in xs[0]:
                 if key != "input_ids" and key != "attention_mask":
                     out[key] = [x[key] for x in xs for _ in range(self.max_num_rollouts)]
+            
+            if self.is_eval:
+                return out
+
+            model_labels = self.tokenizer(labels, truncation=True, padding=False, 
+                            max_length=self.config.max_rollin_length, add_special_tokens=self.add_special_tokens,)
+
+            indexes = [0] * len(prompts_tokens)
+
+            label_tokens = model_labels['input_ids']
+            new_prompts = []
+            indexes = []
+            for (prompt, label) in zip(prompts_tokens, label_tokens):
+                num_label_tokens = len(label)
+                num_rollouts = min(self.config.max_num_rollouts, 
+                                    num_label_tokens // self.config.interval)
+
+                intervals = []
+                start_index = 0
+                end_index = num_label_tokens
+                if self.config.exclude_first:
+                    start_index += 1
+                elif self.config.must_include_first:
+                    intervals.append(0)
+                    start_index += 1
+                    num_rollouts -= 1
+
+                if self.config.exclude_last:
+                    end_index -= 1
+                elif self.config.must_include_last:
+                    intervals.append(end_index)
+                    end_index -= 1
+                    num_rollouts -= 1
+
+                max_rollin_length = min(end_index, self.config.max_rollin_length)
+
+                rollin_intervals = range(start_index, max_rollin_length, self.config.interval)
+                interval_length = len(rollin_intervals)
+                # dist_weights = np.array([np.sin(np.pi * i/(interval_length - 1)/2 + 
+                #                         np.pi * self.curr_step/(self.total_steps - 1)/2)
+                #                         for i in range(interval_length)])
+
+                dist_weights = np.ones(interval_length)
+                if self.config.use_sampling_curriculum:
+                    dist_weights = np.array([np.exp(
+                        (self.config.shifting_frac * self.total_steps - self.curr_step)/self.total_steps * 
+                        (i - interval_length) / interval_length)**self.config.sampling_curr_coeff
+                                        for i in range(interval_length)])
+
+                dist_weights = np.array(dist_weights) / np.sum(dist_weights)
+                intervals += list(np.random.choice(rollin_intervals, num_rollouts, replace=False, p=dist_weights))
+                for l in sorted(intervals):
+                    rollin_prompt_suffix = label[:l]
+                    rollout_prompt = prompt + rollin_prompt_suffix
+                    new_prompts.append(rollout_prompt)
+                    indexes.append(l)
+
+            new_tokenized_prompts = self.tokenizer.pad([{"input_ids": x} for x in  new_prompts], return_tensors="pt")
+
+
+            out['augmented_input_ids'] = new_tokenized_prompts['input_ids']
+            out['augmented_attention_mask'] = new_tokenized_prompts['attention_mask']
+            out['augmented_indexes'] = torch.tensor(indexes)
+
             return out
 
 
