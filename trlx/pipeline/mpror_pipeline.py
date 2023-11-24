0eb3c0a6f3bd31e732180a253d8a7852780c54a0
Kushal Arora <kushal.arora@tri.global>
2023-11-24 10:50:47 -0800

    merge change bullshit.

--- trlx/pipeline/mpror_pipeline.py
+++ trlx/pipeline/mpror_pipeline.py
@@ -97,62 +97,12 @@ class MPRORPipeline(BasePipeline):
             self.curr_step += 1
 
             prompts_tokens = [x["input_ids"] for x in xs]
-            indexes = [0] * len(prompts_tokens)
+            model_labels = self.tokenizer(labels, truncation=True, padding=False, 
+                            max_length=self.config.max_rollin_length, add_special_tokens=self.add_special_tokens,)
 
             label_tokens = model_labels['input_ids']
-            if not self.is_eval:
-                new_prompts = []
-                indexes = []
-                for (prompt, label) in zip(prompts_tokens, label_tokens):
-                    num_label_tokens = len(label)
-                    num_rollouts = min(self.config.max_num_rollouts, 
-                                        num_label_tokens // self.config.interval)
 
-                    intervals = []
-                    start_index = 0
-                    end_index = num_label_tokens
-                    if self.config.exclude_first:
-                        start_index += 1
-                    elif self.config.must_include_first:
-                        intervals.append(0)
-                        start_index += 1
-                        num_rollouts -= 1
-
-                    if self.config.exclude_last:
-                        end_index -= 1
-                    elif self.config.must_include_last:
-                        intervals.append(end_index)
-                        end_index -= 1
-                        num_rollouts -= 1
-
-                    max_rollin_length = min(end_index, self.config.max_rollin_length)
-
-                    rollin_intervals = range(start_index, max_rollin_length, self.config.interval)
-                    interval_length = len(rollin_intervals)
-                    # dist_weights = np.array([np.sin(np.pi * i/(interval_length - 1)/2 + 
-                    #                         np.pi * self.curr_step/(self.total_steps - 1)/2)
-                    #                         for i in range(interval_length)])
-
-                    num_rollouts = min(num_rollouts, interval_length)
-                    
-                    dist_weights = np.ones(interval_length)
-                    if self.config.use_sampling_curriculum:
-                        dist_weights = np.array([np.exp(
-                            (self.config.shifting_frac * self.total_steps - self.curr_step)/self.total_steps * 
-                            (i - interval_length) / interval_length)**self.config.sampling_curr_coeff
-                                            for i in range(interval_length)])
-
-                    dist_weights = np.array(dist_weights) / np.sum(dist_weights)
-                    intervals += list(np.random.choice(rollin_intervals, num_rollouts, replace=False, p=dist_weights))
-                    for l in sorted(intervals):
-                        rollin_prompt_suffix = label[:l]
-                        rollout_prompt = prompt + rollin_prompt_suffix
-                        new_prompts.append(rollout_prompt)
-                        indexes.append(l)
-
-                prompts_tokens = new_prompts
-
-            out = self.tokenizer.pad([{"input_ids": x} for idx, x in zip(indexes, prompts_tokens)], return_tensors="pt")
+            out = self.tokenizer.pad([{"input_ids": x} for x in prompts_tokens for _ in range(self.max_num_rollouts)], return_tensors="pt")
 
             for key in xs[0]:
                 if key != "input_ids" and key != "attention_mask":
@@ -161,12 +111,8 @@ class MPRORPipeline(BasePipeline):
             if self.is_eval:
                 return out
 
-            model_labels = self.tokenizer(labels, truncation=True, padding=False, 
-                            max_length=self.config.max_rollin_length, add_special_tokens=self.add_special_tokens,)
 
-            indexes = [0] * len(prompts_tokens)
 
-            label_tokens = model_labels['input_ids']
             new_prompts = []
             indexes = []
             for (prompt, label) in zip(prompts_tokens, label_tokens):
